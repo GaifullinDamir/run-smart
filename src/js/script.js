e206386c5b4ab45192e3345000b388c22266635c
Name <damiros.20002@gmail.com>
2023-08-17 20:23:50 +0300

    Адаптация слайдера под различные разрешения.

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,28 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const slider = tns({
         container: '.carousel__inner',
         items: 1,
+        speed: 1500,
+        // autoHeight: true,
         slideBy: 'page',
-        autoplay: false,
+        // autoplay: true,
+        // autoplayTimeout: 5000,
+        // autoplayButtonOutput: false,
+        arrowKeys: true,
         nav: false,
-        controls: false
+        controls: false,
+        responsive: {
+            '991': {
+                nav: false
+            },
+            '768': {
+                nav: false
+            },
+            '576': {
+                nav: true
+            },
+            
+          }
+      
       });    
     
     document.querySelector('.arrow-prev').addEventListener('click', (e) => {
